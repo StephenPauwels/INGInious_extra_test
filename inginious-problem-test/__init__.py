8bded6f2cf1247cd9a733cc604e07b3d6a05af54
Stephen <stephen.pauwels@uantwerpen.be>
2021-02-11 12:52:20 +0100

    Init Commit

---
+++ inginious-problem-test/__init__.py
@@ -0,0 +1,71 @@
+# Inginious-problem-test
+
+import os
+
+from inginious.common.tasks_problems import Problem
+from inginious.frontend.task_problems import DisplayableProblem
+from inginious.frontend.parsable_text import ParsableText
+
+PATH_TO_PLUGIN = os.path.abspath(os.path.dirname(__file__))
+PATH_TO_TEMPLATES = os.path.join(PATH_TO_PLUGIN, "templates")
+
+class TestProblem(Problem):
+    """Add a different test set, using code from another problem"""
+
+    def __init__(self, problemid, content, translations, taskfs):
+        super(TestProblem, self).__init__(problemid, content, translations, taskfs)
+        self._header = content['header'] if "header" in content else ""
+        self._optional = True
+
+    @classmethod
+    def get_type(cls):
+        return "extra_test"
+
+    def input_is_consistent(self, task_input, default_allowed_extension, default_max_size):
+        return True
+
+    def input_type(self):
+        return None
+
+    def check_answer(self, _, __):
+        return None, None, None, 0, ""
+
+    @classmethod
+    def parse_problem(self, problem_content):
+        return Problem.parse_problem(problem_content)
+
+    @classmethod
+    def get_text_fields(cls):
+        fields = Problem.get_text_fields()
+        fields.update({"header": True})
+        return fields
+
+class DisplayableTestProblem(TestProblem, DisplayableProblem):
+    """ A displayable match problem """
+
+    def __init__(self, problemid, content, translations, taskfs):
+        super(DisplayableTestProblem, self).__init__(problemid, content, translations, taskfs)
+
+    @classmethod
+    def get_type_name(cls, language):
+        return _("extra_test")
+
+    def show_input(self, template_helper, language, seed):
+        """ Show MatchProblem """
+        header = ParsableText(self.gettext(language, self._header), "rst",
+                              translation=self.get_translation_obj(language))
+        return template_helper.render("extra_test.html", template_folder=PATH_TO_TEMPLATES,
+                                      pid=self.get_id(), header=header)
+
+    @classmethod
+    def show_editbox(cls, template_helper, key, language):
+        return template_helper.render("extra_test_edit.html",
+                                      template_folder=PATH_TO_TEMPLATES,
+                                      key=key)
+
+    @classmethod
+    def show_editbox_templates(cls, template_helper, key, language):
+        return ""
+
+def init(plugin_manager, course_factory, client, plugin_config):
+    course_factory.get_task_factory().add_problem_type(DisplayableTestProblem)
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="inginious-problem-test",
+    version="0.1",
+    description="Plugin to allow extra tests without extra input from student",
+    packages=find_packages(),
+    install_requires=["inginious>=0.6.dev0"],
+    test_require=[],
+    extras_require={},
+    scripts=[],
+    include_package_data=True,
+    author="Stephen Pauwels - UAntwerpen",
+    author_email="stephen.pauwels@uantwerpen.be",
+    license="AGPL 3",
+    url=""
+)
